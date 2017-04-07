89b7376b3645087282f047f8a58a0d8dc7b75034
BenjaminBONTEMPS <benjaminbontemps@gmail.com>
2017-04-07 13:22:58 +0200

    Intégration de la sauvegarde des ROI à la sauvegarde du projet

--- Assets/Scripts/HBP/UI/Project/ProjectLoaderSaver.cs
+++ Assets/Scripts/HBP/UI/Project/ProjectLoaderSaver.cs
@@ -501,6 +501,29 @@ namespace HBP.UI
             yield return Ninja.JumpToUnity;
             HandleError(l_loadingState, additionalInformations, projectDirectory);
 
+            // Copy ROI
+            yield return Ninja.JumpBack;
+            try
+            {
+                string[] roiDirectories = Directory.GetDirectories(l_projectPath + Path.DirectorySeparatorChar + "ROI");
+                foreach (string directory in roiDirectories)
+                {
+                    string newDirectory = l_projectTempPath + Path.DirectorySeparatorChar + directory.Substring(l_projectPath.Length);
+                    Directory.CreateDirectory(newDirectory);
+                    DirectoryInfo dirInfo = new DirectoryInfo(directory);
+                    FileInfo[] oldROIFiles = dirInfo.GetFiles("*.roi");
+                    foreach (FileInfo file in oldROIFiles)
+                    {
+                        file.MoveTo(newDirectory + Path.DirectorySeparatorChar + file.Name);
+                    }
+                }
+            }
+            catch
+            {
+                l_loadingState = SaveErrorTypeEnum.CanNotDeleteOldDirectories;
+            }
+            yield return Ninja.JumpToUnity;
+
             // Deleting old directories.
             loadingCircle.Set((float)l_actualStep / l_maxStep, "Deleting old directories");
             yield return Ninja.JumpBack;
