9ce9e97dacf7a3bdbe7b55bb996b9b779d85c671
BenjaminBONTEMPS <benjaminbontemps@gmail.com>
2017-06-09 17:44:12 +0200

    IEEG Transparency tool

--- Assets/Scripts/HBP/UI/Module3D/Toolbar/IEEGSettingsToolbar.cs
+++ Assets/Scripts/HBP/UI/Module3D/Toolbar/IEEGSettingsToolbar.cs
@@ -13,12 +13,18 @@ namespace HBP.UI.Module3D
         /// </summary>
         [SerializeField]
         private Tools.ThresholdIEEG m_ThresholdIEEG;
+        /// <summary>
+        /// IEEG Transparency parameters
+        /// </summary>
+        [SerializeField]
+        private Tools.IEEGTransparency m_IEEGTransparency;
         #endregion
 
         #region Private Methods
         protected override void AddTools()
         {
             m_Tools.Add(m_ThresholdIEEG);
+            m_Tools.Add(m_IEEGTransparency);
         }
         #endregion
     }
