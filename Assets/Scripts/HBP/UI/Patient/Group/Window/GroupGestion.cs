cb1340fc38636b8b1a0399f270acd2fe9b470f32
AdrienGannerie <adrien.gannerie@gmail.com>
2017-09-13 15:49:07 +0200

    Mise en place des counters des selections et des bouttons de tri sur les fenêtres group gestion et group modifier

--- Assets/Scripts/HBP/UI/Patient/Group/Window/GroupGestion.cs
+++ Assets/Scripts/HBP/UI/Patient/Group/Window/GroupGestion.cs
@@ -1,15 +1,25 @@
 ﻿using System.Linq;
 using HBP.Data;
+using UnityEngine.UI;
 
 namespace HBP.UI.Anatomy
 {
     public class GroupGestion : ItemGestion<Group>
     {
+        #region Properties
+        Text m_groupCounter;
+        #endregion
+
         #region Public Methods
         public override void Save()
         {
             ApplicationState.ProjectLoaded.SetGroups(Items.ToArray());
             base.Save();
+        }
+        public override void Remove()
+        {
+            base.Remove();
+            m_groupCounter.text = m_List.ObjectsSelected.Count().ToString();
         }
         #endregion
 
@@ -19,6 +29,9 @@ namespace HBP.UI.Anatomy
             m_List = transform.Find("Content").Find("Groups").Find("List").Find("Display").Find("Viewport").Find("Content").GetComponent<GroupList>();
             (m_List as GroupList).OnAction.AddListener((item,i) => OpenModifier(item,true));
             AddItem(ApplicationState.ProjectLoaded.Groups.ToArray());
+
+            m_groupCounter = transform.Find("Content").Find("Buttons").Find("ItemSelected").Find("Counter").GetComponent<Text>();
+            m_List.OnSelectionChanged.AddListener((g,b) => m_groupCounter.text = m_List.ObjectsSelected.Count().ToString());
         }
         #endregion
     }
