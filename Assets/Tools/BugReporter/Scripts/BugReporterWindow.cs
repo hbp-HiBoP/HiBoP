d9c3a3142f7dc82a900790fe1c13cdc037de8868
BenjaminBONTEMPS <benjaminbontemps@gmail.com>
2017-10-02 14:07:35 +0200

    Bug reporter window

---
+++ Assets/Tools/BugReporter/Scripts/BugReporterWindow.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Tools.Unity
+{
+    public class BugReporterWindow : MonoBehaviour
+    {
+        #region Properties
+        [SerializeField]
+        private Button m_SubmitButton;
+        [SerializeField]
+        private Button m_CancelButton;
+        [SerializeField]
+        private Button m_CloseButton;
+        [SerializeField]
+        private InputField m_ReporterName;
+        [SerializeField]
+        private InputField m_Description;
+        [SerializeField]
+        private InputField m_MailAdress;
+        #endregion
+
+        #region Events
+        public GenericEvent<bool> OnCloseWindow = new GenericEvent<bool>();
+        #endregion
+
+        #region Private Methods
+        private void Awake()
+        {
+            m_SubmitButton.onClick.AddListener(() =>
+            {
+                SendMail();
+                OnCloseWindow.Invoke(true);
+            });
+            m_CancelButton.onClick.AddListener(() =>
+            {
+                OnCloseWindow.Invoke(false);
+            });
+            m_CloseButton.onClick.AddListener(() =>
+            {
+                OnCloseWindow.Invoke(false);
+            });
+        }
+        private void SendMail()
+        {
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("hibophelp@gmail.com", "Bug Reporter");
+            mail.To.Add("hibophelp@gmail.com");
+            mail.Subject = "BUGREPORT " + DateTime.Now.ToString("yyyy-MM-dd HH:mm");
+            mail.Body = m_ReporterName.text + " " + m_MailAdress.text + "\n\n" + m_Description.text;
+            
+            string logFile = "./HiBoP_Data/output_log.txt";
+            if (File.Exists(logFile)) // windows only; TODO : multi support
+            {
+                string copiedLogFile = "./HiBoP_Data/log.txt";
+                File.Copy(logFile, copiedLogFile, true);
+                mail.Attachments.Add(new Attachment(copiedLogFile));
+            }
+
+            SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+            smtpServer.Port = 587;
+            smtpServer.Credentials = new NetworkCredential("hibophelp@gmail.com", "hibop2017") as ICredentialsByHost;
+            smtpServer.EnableSsl = true;
+            ServicePointManager.ServerCertificateValidationCallback = delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) { return true; };
+            smtpServer.Send(mail);
+        }
+        #endregion
+    }
+}
---
+++ Assets/Tools/BugReporter/Scripts/GlobalExceptionManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tools.Unity
+{
+    public class GlobalExceptionManager : MonoBehaviour // Maybe FIXME : integrate this to the windows system
+    {
+        #region Properties
+        private bool m_WindowOpen = false;
+        [SerializeField]
+        private GameObject m_BugReporterWindowPrefab;
+        #endregion
+
+        #region Private Methods
+        private void OnEnable()
+        {
+            Application.logMessageReceived += HandleException;
+        }
+        private void OnDisable()
+        {
+            Application.logMessageReceived -= HandleException;
+        }
+        private void HandleException(string condition, string stackTrace, LogType type)
+        {
+            if ((type == LogType.Error || type == LogType.Exception) && !m_WindowOpen)
+            {
+                BugReporterWindow window = Instantiate(m_BugReporterWindowPrefab, GameObject.Find("Windows").transform, false).GetComponent<BugReporterWindow>();
+                m_WindowOpen = true;
+                window.OnCloseWindow.AddListener((mailSent) =>
+                {
+                    Destroy(window.gameObject);
+                    if (mailSent)
+                    {
+                        ApplicationState.DialogBoxManager.Open(DialogBoxManager.AlertType.Informational, "Bug report successfully sent.", "The issue will be adressed as soon as possible. If you've entered your contact information, we may contact you for further information concerning the bug you encountered.");
+                    }
+                    m_WindowOpen = false;
+                });
+            }
+        }
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+    }
+}
