69de8c8863d9fbc14aad4f8d9166446a3d075778
BenjaminBONTEMPS <benjaminbontemps@gmail.com>
2018-01-12 14:19:19 +0100

    Améliorations sur le rendu de la liste de sites

--- Assets/Scripts/HBP/UI/Module3D/objects/SiteList/SiteItem.cs
+++ Assets/Scripts/HBP/UI/Module3D/objects/SiteList/SiteItem.cs
@@ -28,47 +28,47 @@ namespace HBP.UI.Module3D
             set
             {
                 base.Object = value;
-
-                m_Site.onClick.RemoveAllListeners();
                 m_Site.GetComponentInChildren<Text>().text = value.Information.Patient.Name + "_" + value.Information.Name;
                 m_Site.interactable = value.IsActive;
-                m_Site.onClick.AddListener(() =>
-                {
-                    ApplicationState.Module3D.SelectedColumn.SelectedSiteID = value.Information.GlobalID;
-                });
-
-                m_Excluded.onValueChanged.RemoveAllListeners();
                 m_Excluded.isOn = value.State.IsExcluded;
-                m_Excluded.onValueChanged.AddListener((isOn) =>
-                {
-                    ApplicationState.Module3D.SelectedScene.ChangeSiteState(isOn ? SiteAction.Exclude : SiteAction.Include, value);
-                    m_Site.interactable = value.IsActive;
-                });
-
-                m_Blacklisted.onValueChanged.RemoveAllListeners();
                 m_Blacklisted.isOn = value.State.IsBlackListed;
-                m_Blacklisted.onValueChanged.AddListener((isOn) =>
-                {
-                    ApplicationState.Module3D.SelectedScene.ChangeSiteState(isOn ? SiteAction.Blacklist : SiteAction.Unblacklist, value);
-                    m_Site.interactable = value.IsActive;
-                });
-
-                m_Marked.onValueChanged.RemoveAllListeners();
                 m_Marked.isOn = value.State.IsMarked;
-                m_Marked.onValueChanged.AddListener((isOn) =>
-                {
-                    ApplicationState.Module3D.SelectedScene.ChangeSiteState(isOn ? SiteAction.Mark : SiteAction.Unmark, value);
-                    m_Site.interactable = value.IsActive;
-                });
-
-                m_Highlighted.onValueChanged.RemoveAllListeners();
                 m_Highlighted.isOn = value.State.IsHighlighted;
-                m_Highlighted.onValueChanged.AddListener((isOn) =>
-                {
-                    ApplicationState.Module3D.SelectedScene.ChangeSiteState(isOn ? SiteAction.Highlight : SiteAction.Unhighlight, value);
-                    m_Site.interactable = value.IsActive;
-                });
             }
+        }
+        #endregion
+
+        #region Private Methods
+        private void Awake()
+        {
+            m_Site.onClick.AddListener(() =>
+            {
+                ApplicationState.Module3D.SelectedColumn.SelectedSiteID = Object.Information.GlobalID;
+            });
+
+            m_Excluded.onValueChanged.AddListener((isOn) =>
+            {
+                ApplicationState.Module3D.SelectedScene.ChangeSiteState(isOn ? SiteAction.Exclude : SiteAction.Include, Object);
+                m_Site.interactable = Object.IsActive;
+            });
+
+            m_Blacklisted.onValueChanged.AddListener((isOn) =>
+            {
+                ApplicationState.Module3D.SelectedScene.ChangeSiteState(isOn ? SiteAction.Blacklist : SiteAction.Unblacklist, Object);
+                m_Site.interactable = Object.IsActive;
+            });
+
+            m_Marked.onValueChanged.AddListener((isOn) =>
+            {
+                ApplicationState.Module3D.SelectedScene.ChangeSiteState(isOn ? SiteAction.Mark : SiteAction.Unmark, Object);
+                m_Site.interactable = Object.IsActive;
+            });
+
+            m_Highlighted.onValueChanged.AddListener((isOn) =>
+            {
+                ApplicationState.Module3D.SelectedScene.ChangeSiteState(isOn ? SiteAction.Highlight : SiteAction.Unhighlight, Object);
+                m_Site.interactable = Object.IsActive;
+            });
         }
         #endregion
     }
