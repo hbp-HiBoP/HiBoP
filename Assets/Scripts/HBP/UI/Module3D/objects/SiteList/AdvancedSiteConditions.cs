a6f7026f55f40fa44a3b1d3d38235e58860217c9
BenjaminBONTEMPS <benjaminbontemps@gmail.com>
2018-06-08 17:06:34 +0200

    Préparation pour les advanced site conditions

--- Assets/Scripts/HBP/UI/Module3D/objects/SiteList/AdvancedSiteConditions.cs
+++ Assets/Scripts/HBP/UI/Module3D/objects/SiteList/AdvancedSiteConditions.cs
@@ -5,60 +5,20 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 namespace HBP.UI.Module3D
 {
-    public class AdvancedSiteConditions : MonoBehaviour // TODO
+    public class AdvancedSiteConditions : BaseSiteConditions
     {
         #region Properties
-        private Base3DScene m_Scene;
-        public GenericEvent<Site> OnApplyActionOnSite = new GenericEvent<Site>();
-        [SerializeField] SiteConditionList m_SiteConditionList;
+        [SerializeField] InputField m_InputField;
         #endregion
 
         #region Public Methods
-        public void Initialize(Base3DScene scene)
+        protected override bool CheckConditions(Site site) // TODO
         {
-            m_Scene = scene;
-            m_SiteConditionList.Initialize();
-        }
-        public void AddCondition()
-        {
-            m_SiteConditionList.Add(new SiteCondition());
-        }
-        public void RemoveSelectedConditions()
-        {
-            SiteCondition[] siteConditions = m_SiteConditionList.ObjectsSelected;
-            foreach (var siteCondition in siteConditions)
-            {
-                m_SiteConditionList.Remove(siteCondition);
-            }
-        }
-        #endregion
-
-        #region Coroutines
-        public IEnumerator c_FindSites(List<Site> sites, UnityEvent onBegin, UnityEvent onEnd, UnityEvent<float> onProgress)
-        {
-            yield return Ninja.JumpToUnity;
-            onBegin.Invoke();
-            int length = sites.Count;
-            for (int i = 0; i < length; ++i)
-            {
-                Site site = sites[i];
-                if (site.Configuration.NormalizedValues.Length > 0)
-                {
-                    yield return Ninja.JumpBack;
-                    bool isSuspicious = m_SiteConditionList.Objects.All(c => c.Check(site));
-                    yield return Ninja.JumpToUnity;
-                    if (isSuspicious)
-                    {
-                        site.State.IsSuspicious = isSuspicious;
-                        OnApplyActionOnSite.Invoke(site);
-                    }
-                    onProgress.Invoke((float)(i + 1) / length);
-                }
-            }
-            onEnd.Invoke();
+            return false;
         }
         #endregion
     }
--- Assets/Scripts/HBP/UI/Module3D/objects/SiteList/BasicSiteConditions.cs
+++ Assets/Scripts/HBP/UI/Module3D/objects/SiteList/BasicSiteConditions.cs
@@ -10,12 +10,9 @@ using UnityEngine.UI;
 
 namespace HBP.UI.Module3D
 {
-    public class BasicSiteConditions : MonoBehaviour
+    public class BasicSiteConditions : BaseSiteConditions
     {
         #region Properties
-        private Base3DScene m_Scene;
-        public GenericEvent<Site> OnApplyActionOnSite = new GenericEvent<Site>();
-
         // State
         [SerializeField] Toggle m_Excluded;
         [SerializeField] Toggle m_NotExcluded;
@@ -62,119 +59,57 @@ namespace HBP.UI.Module3D
         [SerializeField] Toggle m_StandardDeviation;
         [SerializeField] Toggle m_StandardDeviationSuperior;
         [SerializeField] InputField m_StandardDeviationValue;
-
-        private Queue<Site> m_MatchingSites = new Queue<Site>();
-        private bool m_UpdateUI;
-        #endregion
-
-        #region Public Methods
-        public void Initialize(Base3DScene scene)
-        {
-            m_Scene = scene;
-        }
         #endregion
 
         #region Private Methods
-        private void Update()
-        {
-            m_UpdateUI = true;
-        }
         private bool CheckState(Site site)
         {
             bool result = true;
-            if (m_Excluded.isOn) result &= site.State.IsExcluded;
-            if (m_NotExcluded.isOn) result &= !site.State.IsExcluded;
-            if (m_Highlighted.isOn) result &= site.State.IsHighlighted;
-            if (m_NotHighlighted.isOn) result &= !site.State.IsHighlighted;
-            if (m_Blacklisted.isOn) result &= site.State.IsBlackListed;
-            if (m_NotBlacklisted.isOn) result &= !site.State.IsBlackListed;
-            if (m_Marked.isOn) result &= site.State.IsMarked;
-            if (m_NotMarked.isOn) result &= !site.State.IsMarked;
-            if (m_Suspicious.isOn) result &= site.State.IsSuspicious;
-            if (m_NotSuspicious.isOn) result &= !site.State.IsSuspicious;
+            if (m_Excluded.isOn) result &= CheckExcluded(site);
+            if (m_NotExcluded.isOn) result &= !CheckExcluded(site);
+            if (m_Highlighted.isOn) result &= CheckHighlighted(site);
+            if (m_NotHighlighted.isOn) result &= !CheckHighlighted(site);
+            if (m_Blacklisted.isOn) result &= CheckBlacklisted(site);
+            if (m_NotBlacklisted.isOn) result &= !CheckBlacklisted(site);
+            if (m_Marked.isOn) result &= CheckMarked(site);
+            if (m_NotMarked.isOn) result &= !CheckMarked(site);
+            if (m_Suspicious.isOn) result &= CheckSuspicious(site);
+            if (m_NotSuspicious.isOn) result &= !CheckSuspicious(site);
             return result;
         }
         private bool CheckPosition(Site site)
         {
             bool result = true;
-            if (m_InROI.isOn) result &= !site.State.IsOutOfROI;
-            if (m_OutOfROI.isOn) result &= site.State.IsOutOfROI;
-            if (m_InMesh.isOn) result &= m_Scene.ColumnManager.SelectedMesh.SimplifiedBoth.IsPointInside(m_Scene.ColumnManager.SelectedImplantation.RawSiteList, site.Information.GlobalID);
-            if (m_OutOfMesh.isOn) result &= !m_Scene.ColumnManager.SelectedMesh.SimplifiedBoth.IsPointInside(m_Scene.ColumnManager.SelectedImplantation.RawSiteList, site.Information.GlobalID);
-            if (m_OnPlane.isOn) result &= m_Scene.ColumnManager.SelectedImplantation.RawSiteList.IsSiteOnAnyPlane(site, from cut in m_Scene.Cuts select cut as HBP.Module3D.Plane, 1.0f);
-            if (m_NotOnPlane.isOn) result &= !m_Scene.ColumnManager.SelectedImplantation.RawSiteList.IsSiteOnAnyPlane(site, from cut in m_Scene.Cuts select cut as HBP.Module3D.Plane, 1.0f);
+            if (m_InROI.isOn) result &= CheckInROI(site);
+            if (m_OutOfROI.isOn) result &= !CheckInROI(site);
+            if (m_InMesh.isOn) result &= CheckInMesh(site);
+            if (m_OutOfMesh.isOn) result &= !CheckInMesh(site);
+            if (m_OnPlane.isOn) result &= CheckOnPlane(site);
+            if (m_NotOnPlane.isOn) result &= !CheckOnPlane(site);
             return result;
         }
         private bool CheckInformation(Site site)
         {
             bool result = true;
-            if (m_SiteName.isOn) result &= site.Information.ChannelName.ToUpper().Contains(m_SiteNameFilter.text.ToUpper());
-            if (m_Patient.isOn) result &= site.Information.Patient.Name.ToUpper().Contains(m_PatientFilter.text.ToUpper());
-            if (m_MarsAtlas.isOn) result &= ApplicationState.Module3D.MarsAtlasIndex.FullName(site.Information.MarsAtlasIndex).ToLower().Contains(m_MarsAtlasFilter.text.ToLower());
-            if (m_Broadman.isOn) result &= ApplicationState.Module3D.MarsAtlasIndex.BroadmanArea(site.Information.MarsAtlasIndex).ToLower().Contains(m_BroadmanFilter.text.ToLower());
+            if (m_SiteName.isOn) result &= CheckName(site, m_SiteNameFilter.text);
+            if (m_Patient.isOn) result &= CheckPatientName(site, m_PatientFilter.text);
+            if (m_MarsAtlas.isOn) result &= CheckMarsAtlasName(site, m_MarsAtlasFilter.text);
+            if (m_Broadman.isOn) result &= CheckBroadmanAreaName(site, m_BroadmanFilter.text);
             return result;
         }
         private bool CheckValues(Site site)
         {
             bool result = true;
-            if (site.Configuration.NormalizedValues.Length > 0)
-            {
-                if (m_Mean.isOn) result &= CompareValue(site.Configuration.NormalizedValues.Mean(), m_MeanSuperior.isOn, m_MeanValue.text);
-                if (m_Median.isOn) result &= CompareValue(site.Configuration.NormalizedValues.Median(), m_MedianSuperior.isOn, m_MedianValue.text);
-                if (m_Max.isOn) result &= CompareValue(site.Configuration.NormalizedValues.Max(), m_MaxSuperior.isOn, m_MaxValue.text);
-                if (m_Min.isOn) result &= CompareValue(site.Configuration.NormalizedValues.Min(), m_MinSuperior.isOn, m_MinValue.text);
-                if (m_StandardDeviation.isOn) result &= CompareValue(site.Configuration.NormalizedValues.StandardDeviation(), m_StandardDeviationSuperior.isOn, m_StandardDeviationValue.text);
-            }
-            else if (m_Mean.isOn || m_Median.isOn || m_Max.isOn || m_Min.isOn || m_StandardDeviation.isOn)
-            {
-                result = false;
-            }
+            if (m_Mean.isOn) result &= CheckMean(site, m_MeanSuperior.isOn, m_MeanValue.text);
+            if (m_Median.isOn) result &= CheckMedian(site, m_MedianSuperior.isOn, m_MedianValue.text);
+            if (m_Max.isOn) result &= CheckMax(site, m_MaxSuperior.isOn, m_MaxValue.text);
+            if (m_Min.isOn) result &= CheckMin(site, m_MinSuperior.isOn, m_MinValue.text);
+            if (m_StandardDeviation.isOn) result &= CheckStandardDeviation(site, m_StandardDeviationSuperior.isOn, m_StandardDeviationValue.text);
             return result;
         }
-        private bool CompareValue(float value, bool superior, string stringValueToCompare)
+        protected override bool CheckConditions(Site site)
         {
-            float valueToCompare = 0f;
-            if (float.TryParse(stringValueToCompare, out valueToCompare))
-            {
-                return superior ? value > valueToCompare : value < valueToCompare;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        #endregion
-
-        #region Coroutines
-        public IEnumerator c_FindSites(List<Site> sites, UnityEvent onBegin, UnityEvent onEnd, UnityEvent<float> onProgress)
-        {
-            yield return Ninja.JumpToUnity;
-            onBegin.Invoke();
-            yield return Ninja.JumpBack;
-            int length = sites.Count;
-            for (int i = 0; i < length; ++i)
-            {
-                Site site = sites[i];
-                bool match = CheckState(site) && CheckPosition(site) && CheckInformation(site) && CheckValues(site);
-                if (match)
-                {
-                    m_MatchingSites.Enqueue(site);
-                }
-                if (m_UpdateUI || i == length - 1)
-                {
-                    yield return Ninja.JumpToUnity;
-                    while (m_MatchingSites.Count > 0)
-                    {
-                        Site siteToApply = m_MatchingSites.Dequeue();
-                        OnApplyActionOnSite.Invoke(siteToApply);
-                    }
-                    onProgress.Invoke((float)(i + 1) / length);
-                    m_UpdateUI = false;
-                    yield return Ninja.JumpBack;
-                }
-            }
-            yield return Ninja.JumpToUnity;
-            onEnd.Invoke();
+            return CheckState(site) && CheckPosition(site) && CheckInformation(site) && CheckValues(site);
         }
         #endregion
     }
