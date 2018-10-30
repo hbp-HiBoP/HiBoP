c00666551daeffde2012f002b86515727d90da89
DESKTOP-EU4VHUQ\Adrien <adrien.gannerie@gmail.com>
2018-10-30 15:43:59 +0100

    Premier passage sur la classe "UI.Bloc" des matrices d'essais.

--- Assets/Scripts/HBP/UI/Others/Informations/Site/TrialMatrix/Bloc.cs
+++ Assets/Scripts/HBP/UI/Others/Informations/Site/TrialMatrix/Bloc.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.Events;
 using System.Collections.Generic;
-using d = HBP.Data.TrialMatrix;
+using data = HBP.Data.TrialMatrix;
 using System.Collections.ObjectModel;
 using System.Linq;
+using UnityEngine.UI;
+using Tools.Unity;
 using UnityEngine.EventSystems;
 
 namespace HBP.UI.TrialMatrix
@@ -12,8 +13,10 @@ namespace HBP.UI.TrialMatrix
     public class Bloc : MonoBehaviour
     {
         #region Properties
-        public GameObject SubBlocPrefab;
-        public RectTransform SubBlocsRectTransform;
+        public data.Bloc Data { get; }
+
+        List<SubBloc> m_SubBlocs = new List<SubBloc>();
+        public ReadOnlyCollection<SubBloc> SubBlocs { get { return new ReadOnlyCollection<SubBloc>(m_SubBlocs); } }
 
         string m_title;
         public string Title
@@ -28,31 +31,24 @@ namespace HBP.UI.TrialMatrix
         public StringEvent OnChangeTitle;
 
         List<int> m_SelectedTrials = new List<int>();
-        public int[] SelectedTrials
-        {
-            get
-            {
-                return m_SelectedTrials.ToArray();
-            }
-            set
-            {
-                m_SelectedTrials = value.ToList();
-            }
-        }
-        //int m_BeginDragTrial;
-        //bool m_Dragging;
-        //[SerializeField] GameObject m_SelectionMask;
+        public ReadOnlyCollection<int> SelectedTrials { get { return new ReadOnlyCollection<int>(m_SelectedTrials); } }
 
-        List<SubBloc> m_SubBlocs = new List<SubBloc>();
-        public ReadOnlyCollection<SubBloc> SubBlocs { get { return new ReadOnlyCollection<SubBloc>(m_SubBlocs); } }
-
+        [SerializeField] GameObject m_SubBlocPrefab;
+        [SerializeField] RectTransform m_SubBlocContainer;
+        [SerializeField] GameObject m_SelectionMaskPrefab;
+        [SerializeField] RectTransform m_SelectionMaskContainer;
+        List<GameObject> m_SelectionMasks;
+        RectTransform m_RectTransform;
+        LayoutElement m_LayoutElement;
+        int m_BeginDragTrial;
+        bool m_Dragging;
         #endregion
 
         #region Public Methods
-        public void Set(d.Bloc bloc,Texture2D colorMap,Vector2 limits)
+        public void Set(data.Bloc bloc,Texture2D colorMap,Vector2 limits)
         {
             Title = bloc.ProtocolBloc.Name;
-            foreach (d.SubBloc subBloc in bloc.SubBlocs)
+            foreach (data.SubBloc subBloc in bloc.SubBlocs)
             {
                 AddSubBloc(subBloc, colorMap, limits);
             }
@@ -68,145 +64,98 @@ namespace HBP.UI.TrialMatrix
         }
         public void SelectTrials(int[] trials,bool additive)
         {
-            //int NumberOfSubTrials = m_Data.SubTrials.Length;
-            //int[] unselectedSubTrials = new int[NumberOfSubTrials - subTrials.Length];
-            //for (int t = 0, i = 0; t < m_Data.SubTrials.Length; t++)
-            //{
-            //    if (!subTrials.Contains(t))
-            //    {
-            //        unselectedSubTrials[i] = t;
-            //        i++;
-            //    }
-            //}
+            if (!additive) m_SelectedTrials.Clear();
+            m_SelectedTrials.AddRange(trials.Where(index => !m_SelectedTrials.Contains(index)));
+            m_SelectedTrials.Sort();
+            ClearSelectionMask();
 
-            //List<List<int>> subTrialsGroups = new List<List<int>>();
-            //List<int> subTrialGroup = new List<int>();
-            //for (int i = 0; i < unselectedSubTrials.Length; i++)
-            //{
-            //    int subTrial = unselectedSubTrials[i];
-            //    if (subTrialGroup.Count != 0 && unselectedSubTrials[i] != subTrialGroup[subTrialGroup.Count - 1] + 1)
-            //    {
-            //        subTrialsGroups.Add(new List<int>(subTrialGroup));
-            //        subTrialGroup = new List<int>();
-            //    }
-            //    subTrialGroup.Add(unselectedSubTrials[i]);
-            //    if (i == unselectedSubTrials.Length - 1)
-            //    {
-            //        subTrialsGroups.Add(new List<int>(subTrialGroup));
-            //    }
-            //}
-
-            //ClearMask();
-            //foreach (var g in subTrialsGroups)
-            //{
-            //    AddMask(g.ToArray());
-            //}
+            int numberOfSelectedTrials = m_SelectedTrials.Count;
+            List<int> group = new List<int>();
+            for (int i = 0; i < numberOfSelectedTrials; i++)
+            {
+                int trial = m_SelectedTrials[i];
+                if (group.Count != 0 && trial != group[group.Count - 1] + 1)
+                {
+                    AddSelectionMask(group.ToArray());
+                    group.Clear();
+                }
+                group.Add(trial);
+                if (i == numberOfSelectedTrials - 1) AddSelectionMask(group.ToArray());
+            }
         }
-        //public void OnPointerDown()
-        //{
-        //    m_BeginDragTrial = GetTrialAtPosition(Input.mousePosition);
-        //}
-        //public void OnPointerClick(BaseEventData p)
-        //{
-        //    PointerEventData pointer = (PointerEventData)p;
-        //    if (pointer.button == PointerEventData.InputButton.Left)
-        //    {
-        //        if (!m_Dragging)
-        //        {
-        //            int[] linesClicked = new int[1] { m_BeginDragTrial };
-        //            if (Input.GetKey(KeyCode.LeftShift))
-        //            {
-        //                SendMessageSelectLines(linesClicked, true);
-        //            }
-        //            else
-        //            {
-        //                SendMessageSelectLines(linesClicked, false);
-        //            }
-        //        }
-        //    }
-        //    else if (pointer.button == PointerEventData.InputButton.Right)
-        //    {
-        //        int max = Data.SubBlocs.Length;
-        //        int[] array = new int[max];
-        //        for (int i = 0; i < max; i++)
-        //        {
-        //            array[i] = i;
-        //        }
-        //        SendMessageSelectLines(array, false);
-        //    }
-        //}
-        //public void OnBeginDrag()
-        //{
-        //    m_Dragging = true;
-        //}
-        //public void OnEndDrag()
-        //{
-        //    int l_endDragLine = Mathf.Clamp(GetTrialAtPosition(Input.mousePosition), 0, m_Data.SubBlocs.Length - 1);
-        //    int l_beginDragLine = Mathf.Clamp(m_BeginDragTrial, 0, m_Data.SubBlocs.Length - 1); ;
-        //    List<int> linesSelected = new List<int>();
-        //    if (l_beginDragLine > l_endDragLine)
-        //    {
-        //        for (int l = l_endDragLine; l <= l_beginDragLine; l++)
-        //        {
-        //            linesSelected.Add(l);
-        //        }
-        //    }
-        //    else
-        //    {
-        //        for (int l = l_beginDragLine; l <= l_endDragLine; l++)
-        //        {
-        //            linesSelected.Add(l);
-        //        }
-        //    }
-        //    if (Input.GetKey(KeyCode.LeftShift))
-        //    {
-        //        SendMessageSelectLines(linesSelected.ToArray(), true);
-
-        //    }
-        //    else
-        //    {
-        //        SendMessageSelectLines(linesSelected.ToArray(), false);
-        //    }
-        //    m_Dragging = false;
-        //}
-        //public void OnScroll()
-        //{
-        //    float l_input = Input.GetAxis("Mouse ScrollWheel") * 10;
-        //    int delta = Mathf.RoundToInt(l_input);
-        //    if (delta != 0)
-        //    {
-        //        int[] l_lines = m_selectedLines.ToArray();
-        //        int size = l_lines.Length - 1;
-        //        if (size < 0) size = 0;
-        //        List<int> l_linesToSelect = new List<int>(size);
-        //        int newLine;
-        //        for (int i = 0; i < l_lines.Length; i++)
-        //        {
-        //            newLine = (((l_lines[i] + delta) % Data.SubBlocs.Length) + Data.SubBlocs.Length) % Data.SubBlocs.Length;
-        //            if (newLine >= 0 && newLine < Data.SubBlocs.Length)
-        //            {
-        //                l_linesToSelect.Add(newLine);
-        //            }
-        //        }
-        //        if (Input.GetKey(KeyCode.LeftShift))
-        //        {
-        //            SendMessageSelectLines(l_linesToSelect.ToArray(), true);
-        //        }
-        //        else
-        //        {
-        //            SendMessageSelectLines(l_linesToSelect.ToArray(), false);
-        //        }
-        //    }
-        //}
+        public void OnPointerDown()
+        {
+            m_BeginDragTrial = GetTrialAtPosition(Input.mousePosition);
+        }
+        public void OnPointerClick(BaseEventData p)
+        {
+            PointerEventData pointer = (PointerEventData)p;
+            if (pointer.button == PointerEventData.InputButton.Left)
+            {
+                if (!m_Dragging)
+                {
+                    SelectTrials(new int[] { m_BeginDragTrial }, Input.GetKey(KeyCode.LeftShift));
+                }
+            }
+            else if (pointer.button == PointerEventData.InputButton.Right)
+            {
+                int numberOfTrials = Data.SubBlocs[0].SubTrials.Length;
+                int[] array = new int[numberOfTrials];
+                for (int i = 0; i < numberOfTrials; i++) array[i] = i;
+                SelectTrials(array, false);
+            }
+        }
+        public void OnBeginDrag()
+        {
+            m_Dragging = true;
+        }
+        public void OnEndDrag()
+        {
+            int numberOfTrials = Data.SubBlocs[0].SubTrials.Length;
+            int beginTrial = Mathf.Clamp(m_BeginDragTrial, 0, numberOfTrials - 1); ;
+            int endTrial = Mathf.Clamp(GetTrialAtPosition(Input.mousePosition), 0, numberOfTrials - 1);
+            List<int> selectedTrials = new List<int>(Mathf.Abs(endTrial - beginTrial));
+            if (beginTrial > endTrial)
+            {
+                for (int i = endTrial; i <= beginTrial; i++)
+                {
+                    selectedTrials.Add(i);
+                }
+            }
+            else
+            {
+                for (int i = beginTrial; i <= endTrial; i++)
+                {
+                    selectedTrials.Add(i);
+                }
+            }
+            SelectTrials(selectedTrials.ToArray(), Input.GetKey(KeyCode.LeftShift));
+            m_Dragging = false;
+        }
+        public void OnScroll()
+        {
+            int delta = Mathf.RoundToInt(Input.GetAxis("Mouse ScrollWheel") * 10);
+            if (delta != 0)
+            {
+                int numberOfTrials = Data.SubBlocs[0].SubTrials.Length;
+                IEnumerable<int> trialsToSelect = m_SelectedTrials.Select(t => Mathf.Clamp(t + delta, 0, numberOfTrials - 1)).Distinct();
+                SelectTrials(trialsToSelect.ToArray(), Input.GetKey(KeyCode.LeftShift));  
+            }
+        }
         #endregion
 
-        #region Private Methods     
+        #region Private Methods    
+        void Awake()
+        {
+            m_LayoutElement = GetComponent<LayoutElement>();
+            m_RectTransform = GetComponent<RectTransform>();
+        }
         void SetSize()
         {
             //switch (ApplicationState.UserPreferences.Visualization.TrialMatrix.SubBlocFormat)
             //{
             //    case HBP.Data.Enums.BlocFormatType.TrialHeight:
-            //        m_LayoutElement.preferredHeight = ApplicationState.UserPreferences.Visualization.TrialMatrix.TrialHeight * m_Data.SubTrials.Length;
+            //        m_LayoutElement.preferredHeight = ApplicationState.UserPreferences.Visualization.TrialMatrix.TrialHeight * Data.SubBlocs[0].SubTrials.Length;
             //        break;
             //    case HBP.Data.Enums.BlocFormatType.TrialRatio:
             //        m_LayoutElement.preferredHeight = ApplicationState.UserPreferences.Visualization.TrialMatrix.TrialRatio * m_RectTransform.rect.width * m_Data.SubTrials.Length;
@@ -216,35 +165,39 @@ namespace HBP.UI.TrialMatrix
             //        break;
             //}
         }
-        void AddSubBloc(d.SubBloc data, Texture2D colorMap, Vector2 limits)
+        void AddSubBloc(data.SubBloc data, Texture2D colorMap, Vector2 limits)
         {
-            SubBloc subBloc = (Instantiate(SubBlocPrefab, SubBlocsRectTransform) as GameObject).GetComponent<SubBloc>();
+            SubBloc subBloc = (Instantiate(m_SubBlocPrefab, m_SubBlocContainer) as GameObject).GetComponent<SubBloc>();
             subBloc.Set(data, colorMap, limits);
             m_SubBlocs.Add(subBloc);
         }
-        //void AddFiller()
-        //{
-        //    GameObject filler = Instantiate(new GameObject("Filler", new System.Type[] { typeof(Image) }), SubBlocsRectTransform);
-        //    filler.GetComponent<Image>().color = Color.black;
-        //}
-        //void AddMask(int[] lines)
-        //{
-        //    RectTransform rectTransform = GetComponent<RectTransform>();
-        //    GameObject displayer = Instantiate(m_SelectionMask as GameObject);
-        //    RectTransform displayerRect = displayer.GetComponent<RectTransform>();
-        //    displayerRect.SetParent(transform.GetChild(1));
-        //    int nbLines = m_Data.SubBlocs.Length;
-        //    float top = (float)(lines[lines.Length - 1] + 1) / nbLines;
-        //    float bot = (float)lines[0] / nbLines;
-        //    displayerRect.anchorMin = new Vector2(0, bot);
-        //    displayerRect.anchorMax = new Vector2(1, top);
-        //    displayerRect.offsetMin = new Vector2(0, 0);
-        //    displayerRect.offsetMax = new Vector2(0, 0);
-        //}
-        //void ClearMask()
-        //{
-        //    foreach (Transform child in transform.GetChild(1)) Destroy(child.gameObject);
-        //}
+        void AddFiller()
+        {
+            GameObject filler = Instantiate(new GameObject("Filler", new System.Type[] { typeof(Image) }), m_SubBlocContainer);
+            filler.GetComponent<Image>().color = Color.black;
+        }
+        void AddSelectionMask(int[] trials)
+        {
+            RectTransform rectTransform = Instantiate(m_SelectionMaskPrefab, m_SelectionMaskContainer).GetComponent<RectTransform>();
+            int numberOfTrials = Data.SubBlocs[0].SubTrials.Length;
+            float yMax = (float) (trials[trials.Length - 1] + 1) / numberOfTrials;
+            float yMin = (float) trials[0] / numberOfTrials;
+            rectTransform.anchorMin = new Vector2(0, yMin);
+            rectTransform.anchorMax = new Vector2(1, yMax);
+            rectTransform.offsetMin = new Vector2(0, 0);
+            rectTransform.offsetMax = new Vector2(0, 0);
+            m_SelectionMasks.Add(rectTransform.gameObject);
+        }
+        void ClearSelectionMask()
+        {
+            foreach (var selectionMask in m_SelectionMasks) Destroy(selectionMask);
+            m_SelectionMasks.Clear();
+        }
+        int GetTrialAtPosition(Vector3 position)
+        {
+            Vector2 ratio = m_RectTransform.GetRatioPosition(position);
+            return Mathf.FloorToInt((1-ratio.y) * Data.SubBlocs[0].SubTrials.Length);
+        }
         #endregion
     }
 }
