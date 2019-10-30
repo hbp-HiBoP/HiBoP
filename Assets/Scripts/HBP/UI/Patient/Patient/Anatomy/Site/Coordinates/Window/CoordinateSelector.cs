4cf09858da13927c3c789a6c4ccfbe05be736806
AdrienGannerie <adrien.gannerie@gmail.com>
2019-10-30 12:15:19 +0100

    Changement sur les coordinates

---
+++ Assets/Scripts/HBP/UI/Patient/Patient/Anatomy/Site/Coordinates/Window/CoordinateSelector.cs
@@ -0,0 +1,14 @@
+﻿using HBP.Data.Anatomy;
+using Tools.Unity.Lists;
+using UnityEngine;
+
+namespace HBP.UI.Anatomy
+{
+    public class CoordinateSelector : ObjectSelector<Coordinate>
+    {
+        #region Properties
+        [SerializeField] CoordinateList m_List;
+        protected override SelectableList<Coordinate> List => m_List;
+        #endregion
+    }
+}
