3d0a7b412f63d73f7e7abab4d3a613d0ed04dd60
AdrienGannerie <adrien.gannerie@gmail.com>
2019-10-30 12:10:25 +0100

    Changement sur les Alias

---
+++ Assets/Scripts/HBP/UI/Aliases/AliasListGestion.cs
@@ -0,0 +1,17 @@
+﻿using Tools.Unity.Components;
+using Tools.Unity.Lists;
+using UnityEngine;
+
+namespace HBP.UI.Alias
+{
+    public class AliasListGestion : ListGestion<Data.Preferences.Alias>
+    {
+        #region Properties
+        [SerializeField] AliasList m_List;
+        public override SelectableListWithItemAction<Data.Preferences.Alias> List => m_List;
+
+        [SerializeField] AliasCreator m_ObjectCreator;
+        public override ObjectCreator<Data.Preferences.Alias> ObjectCreator => m_ObjectCreator;
+        #endregion
+    }
+}
