d590d665d46ed8b9a2624acd061df02554b98256
AdrienGannerie <adrien.gannerie@gmail.com>
2019-10-30 11:48:04 +0100

    Changement sur les events

---
+++ Assets/Scripts/HBP/UI/Experience/Protocol/Bloc/SubBloc/Event/EventCreator.cs
@@ -0,0 +1,15 @@
+﻿using p = HBP.Data.Experience.Protocol;
+using Tools.Unity.Components;
+
+namespace HBP.UI.Experience.Protocol
+{
+    public class EventCreator : ObjectCreator<p.Event>
+    {
+        public Data.Enums.MainSecondaryEnum Type;
+
+        public override void CreateFromScratch()
+        {
+            OpenModifier(new p.Event(Type));
+        }
+    }
+}
--- Assets/Scripts/HBP/UI/Experience/Protocol/Bloc/SubBloc/Event/EventSelector.cs
+++ Assets/Scripts/HBP/UI/Experience/Protocol/Bloc/SubBloc/Event/EventSelector.cs
@@ -1,21 +1,15 @@
 ﻿using HBP.UI.Experience.Protocol;
 using d = HBP.Data.Experience.Protocol;
 using UnityEngine;
+using Tools.Unity.Lists;
 
 namespace HBP.UI
 {
     public class EventSelector : ObjectSelector<d.Event>
     {
         #region Properties
-        [SerializeField] EventList m_EventList;
-        #endregion
-
-        #region Public Methods
-        protected override void Initialize()
-        {
-            m_List = m_EventList;
-            base.Initialize();
-        }
+        [SerializeField] EventList m_List;
+        protected override SelectableList<d.Event> List => m_List;
         #endregion
     }
 }
