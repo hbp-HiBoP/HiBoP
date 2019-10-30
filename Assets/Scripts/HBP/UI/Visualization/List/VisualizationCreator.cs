aed73682b76f1bbd797190c61d395d18841ab151
AdrienGannerie <adrien.gannerie@gmail.com>
2019-10-30 12:03:05 +0100

    Changement sur les visualizations

---
+++ Assets/Scripts/HBP/UI/Visualization/List/VisualizationCreator.cs
@@ -0,0 +1,8 @@
+﻿using Tools.Unity.Components;
+
+namespace HBP.UI.Visualization
+{
+    public class VisualizationCreator : ObjectCreator<Data.Visualization.Visualization>
+    {
+    }
+}
---
+++ Assets/Scripts/HBP/UI/Visualization/List/VisualizationListGestion.cs
@@ -0,0 +1,17 @@
+﻿using Tools.Unity.Components;
+using Tools.Unity.Lists;
+using UnityEngine;
+
+namespace HBP.UI.Visualization
+{
+    public class VisualizationListGestion : ListGestion<Data.Visualization.Visualization>
+    {
+        #region Properties
+        [SerializeField] VisualizationList m_List;
+        public override SelectableListWithItemAction<Data.Visualization.Visualization> List => m_List;
+
+        [SerializeField] VisualizationCreator m_ObjectCreator;
+        public override ObjectCreator<Data.Visualization.Visualization> ObjectCreator => m_ObjectCreator;
+        #endregion
+    }
+}
