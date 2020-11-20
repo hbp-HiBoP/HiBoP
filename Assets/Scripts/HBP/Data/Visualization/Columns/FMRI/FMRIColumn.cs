d3207e0f45665a49972602da00e5cab50230d1da
BenjaminBONTEMPS <benjaminbontemps@gmail.com>
2020-11-20 16:25:17 +0100

    Column FMRI côté data OK

---
+++ Assets/Scripts/HBP/Data/Visualization/Columns/FMRI/FMRIColumn.cs
@@ -0,0 +1,81 @@
+﻿using HBP.Data.Experience.Dataset;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace HBP.Data.Visualization
+{
+    [DataContract, DisplayName("FMRI")]
+    public class FMRIColumn : Column
+    {
+        #region Properties
+        [DataMember(Name = "Dataset")] string datasetID;
+        /// <summary>
+        /// Dataset of the column.
+        /// </summary>
+        public Dataset Dataset
+        {
+            get
+            {
+                return ApplicationState.ProjectLoaded.Datasets.FirstOrDefault(p => p.ID == datasetID);
+            }
+            set
+            {
+                if (value == null)
+                {
+                    datasetID = string.Empty;
+                }
+                else
+                {
+                    datasetID = value.ID;
+                }
+            }
+        }
+
+        [DataMember] public FMRIConfiguration FMRIConfiguration { get; set; }
+        #endregion
+
+        #region Constructors
+        public FMRIColumn(string name, BaseConfiguration baseConfiguration, Dataset dataset, FMRIConfiguration fmriConfiguration, string ID) : base(name, baseConfiguration, ID)
+        {
+            FMRIConfiguration = fmriConfiguration;
+            Dataset = dataset;
+        }
+        public FMRIColumn(string name, BaseConfiguration baseConfiguration, Dataset dataset, FMRIConfiguration fmriConfiguration) : base(name, baseConfiguration)
+        {
+            FMRIConfiguration = fmriConfiguration;
+            Dataset = dataset;
+        }
+        public FMRIColumn(string name, BaseConfiguration baseConfiguration) : this(name, baseConfiguration, null, new FMRIConfiguration())
+        {
+        }
+        public FMRIColumn() : this("", new BaseConfiguration())
+        {
+        }
+        #endregion
+
+        #region Public Methods
+        public override object Clone()
+        {
+            return new FMRIColumn(Name, BaseConfiguration.Clone() as BaseConfiguration, Dataset, FMRIConfiguration.Clone() as FMRIConfiguration, ID);
+        }
+        public override void Copy(object copy)
+        {
+            base.Copy(copy);
+            if(copy is FMRIColumn fmriColumn)
+            {
+                FMRIConfiguration = fmriColumn.FMRIConfiguration;
+                Dataset = fmriColumn.Dataset;
+            }
+        }
+        public override bool IsCompatible(IEnumerable<Patient> patients)
+        {
+            return true;
+        }
+        public override void Unload()
+        {
+        }
+        #endregion  
+    }
+}
---
+++ Assets/Scripts/HBP/Data/Visualization/Columns/FMRI/FMRIConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Serialization;
+
+namespace HBP.Data.Visualization
+{
+    [DataContract]
+    public class FMRIConfiguration : BaseData
+    {
+        #region Properties
+        #endregion
+
+        #region Constructors
+        public FMRIConfiguration() : base()
+        {
+
+        }
+        public FMRIConfiguration(string ID) : base(ID)
+        {
+
+        }
+        #endregion
+
+        #region Public Methods
+        public override object Clone()
+        {
+            return new FMRIConfiguration(ID);
+        }
+        #endregion
+
+        #region Private Methods
+        #endregion
+    }
+}
