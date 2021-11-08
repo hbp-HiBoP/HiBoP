26c44f9920a319173edcea8fc044c547446ccee1
BenjaminBONTEMPS <benjaminbontemps@gmail.com>
2021-11-08 16:53:39 +0100

    Export de certaines données présentes dans les projets

--- Assets/Scripts/HBP/UI/Others/Window/GestionWindow.cs
+++ Assets/Scripts/HBP/UI/Others/Window/GestionWindow.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.IO;
+using Tools.Unity;
 using Tools.Unity.Components;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace HBP.UI
 {
@@ -15,6 +19,8 @@ namespace HBP.UI
         /// </summary>
         public abstract ListGestion<T> ListGestion { get; }
 
+        [SerializeField] protected Button m_ExportButton;
+
         public override bool Interactable
         {
             get => base.Interactable;
@@ -23,6 +29,7 @@ namespace HBP.UI
                 base.Interactable = value;
                 ListGestion.Interactable = value;
                 ListGestion.Modifiable = value;
+                SetExport();
             }
         }
         #endregion
@@ -32,6 +39,29 @@ namespace HBP.UI
         {
             base.Initialize();
             ListGestion.WindowsReferencer.OnOpenWindow.AddListener(window => WindowsReferencer.Add(window));
+            m_ExportButton.onClick.AddListener(ExportSelected);
+            ListGestion.List.OnSelect.AddListener((obj) => SetExport());
+            ListGestion.List.OnDeselect.AddListener((obj) => SetExport());
+        }
+        protected void ExportSelected()
+        {
+            string directory = FileBrowser.GetExistingDirectoryName();
+            if (string.IsNullOrEmpty(directory)) return;
+
+            var selectedObjects = ListGestion.List.ObjectsSelected;
+            foreach (var selectedObject in selectedObjects)
+            {
+                if (selectedObject is ILoadable<T> loadable && selectedObject is INameable nameable)
+                {
+                    ClassLoaderSaver.SaveToJSon(selectedObject, Path.Combine(directory, string.Format("{0}.{1}", nameable.Name, loadable.GetExtensions()[0])));
+                }
+            }
+            ApplicationState.DialogBoxManager.Open(DialogBoxManager.AlertType.Informational, "Selected items have been saved", string.Format("{0} items have been saved at {1}.", selectedObjects.Length, directory));
+        }
+        protected void SetExport()
+        {
+            var selectedObjects = ListGestion.List.ObjectsSelected;
+            m_ExportButton.interactable = selectedObjects.Length > 0 && Interactable;
         }
         #endregion
     }
