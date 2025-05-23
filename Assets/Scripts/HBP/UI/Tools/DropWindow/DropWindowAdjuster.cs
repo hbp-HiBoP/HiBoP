3f5249a512b527a7705267bc74d14e3b3489f1c2
Benjamin BONTEMPS <benjaminbontemps@gmail.com>
2025-05-23 19:15:09 +0200

    Fix adjuster

--- Assets/Scripts/HBP/UI/Tools/DropWindow/DropWindowAdjuster.cs
+++ Assets/Scripts/HBP/UI/Tools/DropWindow/DropWindowAdjuster.cs
@@ -7,48 +7,82 @@ namespace HBP.UI.Tools
     public class DropWindowAdjuster : MonoBehaviour
     {
         #region Properties
-        private RectTransform m_RectTransfom;
+        private RectTransform m_RectTransform;
+        private RectTransform m_Canvas;
+
+        private float m_OldHeight;
         #endregion
 
         #region Private Methods
         private void Awake()
         {
-            m_RectTransfom = GetComponent<RectTransform>();
+            m_RectTransform = GetComponent<RectTransform>();
+            m_Canvas = m_RectTransform.GetTopmostCanvas().GetComponent<RectTransform>();
         }
-        public void Update()
+        private void Update()
         {
-            if (m_RectTransfom.hasChanged)
+            if (m_OldHeight != m_RectTransform.rect.height)
             {
                 AdjustIfOutOfScreen();
-                m_RectTransfom.hasChanged = false;
+                m_RectTransform.hasChanged = false;
             }
+        }
+        private void OnEnable()
+        {
+            AdjustIfOutOfScreen();
+        }
+        private void OnDisable()
+        {
+            ApplyBottomPosition();
         }
         private void AdjustIfOutOfScreen()
         {
-            // TODO
-            float value = m_RectTransfom.position.y - m_RectTransfom.rect.height;
-            if (value < 0)
+            if (CheckLimits())
             {
-                m_RectTransfom.anchorMin = new Vector2(m_RectTransfom.anchorMin.x, 1);
-                m_RectTransfom.anchorMax = new Vector2(m_RectTransfom.anchorMax.x, 1);
-                m_RectTransfom.pivot = new Vector2(m_RectTransfom.pivot.x, 0);
-
-                Vector2 newPos = m_RectTransfom.anchoredPosition;
-                newPos.y = 1;
-                m_RectTransfom.anchoredPosition = newPos;
+                ApplyTopPosition();
             }
             else
             {
-                m_RectTransfom.anchorMin = new Vector2(m_RectTransfom.anchorMin.x, 0);
-                m_RectTransfom.anchorMax = new Vector2(m_RectTransfom.anchorMax.x, 0);
-                m_RectTransfom.pivot = new Vector2(m_RectTransfom.pivot.x, 1);
-
-                Vector2 newPos = m_RectTransfom.anchoredPosition;
-                newPos.y = -1;
-                m_RectTransfom.anchoredPosition = newPos;
+                ApplyBottomPosition();
+                if (CheckLimits())
+                {
+                    ApplyTopPosition();
+                }
             }
-        }
 
+            m_OldHeight = m_RectTransform.rect.height;
+        }
+        private bool CheckLimits()
+        {
+            Vector3[] worldCorners = new Vector3[4];
+            m_RectTransform.GetWorldCorners(worldCorners);
+
+            Vector3[] canvasCorners = new Vector3[4];
+            m_Canvas.GetWorldCorners(canvasCorners);
+
+            float windowBottom = worldCorners[0].y;
+            float canvasBottom = canvasCorners[0].y;
+
+            return windowBottom < canvasBottom;
+        }
+        private void ApplyTopPosition()
+        {
+            m_RectTransform.anchorMin = new Vector2(m_RectTransform.anchorMin.x, 1);
+            m_RectTransform.anchorMax = new Vector2(m_RectTransform.anchorMax.x, 1);
+            m_RectTransform.pivot = new Vector2(m_RectTransform.pivot.x, 0);
+            Vector2 newPos = m_RectTransform.anchoredPosition;
+            newPos.y = 1;
+            m_RectTransform.anchoredPosition = newPos;
+        }
+        private void ApplyBottomPosition()
+        {
+            m_RectTransform.anchorMin = new Vector2(m_RectTransform.anchorMin.x, 0);
+            m_RectTransform.anchorMax = new Vector2(m_RectTransform.anchorMax.x, 0);
+            m_RectTransform.pivot = new Vector2(m_RectTransform.pivot.x, 1);
+            Vector2 newPos = m_RectTransform.anchoredPosition;
+            newPos.y = -1;
+            m_RectTransform.anchoredPosition = newPos;
+        }
         #endregion
     }
 }
